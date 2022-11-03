48279d363bc1db05f2c8d45d6cccf3099eee69ff
Jaimes Nel <jaimes@macbook-pro-2.local>
2022-11-03 02:00:34 +0000

    Added hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Hero = ({ children }) => {
+  return (
+	<>
+	<div className="bg-blue/10 p-20">
+	<h1>Preparing a ClimateTech investment pitch?</h1> 
+	<h2>Pitchmark helps ClimateTech founders prepare, practice, and deliver pitches that win investment.</h2>
+	</div>
+	</>
+  )
+}
+
+export default Hero
--- src/components/pagestructure/Header.js
+++ src/components/pagestructure/Header.js
@@ -3,7 +3,7 @@ import Container from "../../components/pagestructure/Container"
 
 const Header = () => (
 	<Container>
-		<p className="text-xl mb-16 bg-black/10 p-10">Pitchmark</p>
+		<p className="text-xl mb-16 bg-black/10 p-10 font-bold">pitchmark</p>
 	</Container>    
 )
 
