823a40233b2fc74ace02cc7e9e0507873f94a4c9
Jaimes Nel <jaimes@macbook-pro-2.local>
2022-11-16 00:52:03 +0000

    Updated completion and calendly link

--- src/components/forms/PitchReadinessForm.js
+++ src/components/forms/PitchReadinessForm.js
@@ -61,15 +61,25 @@ const presentationratings = [
   ]
   
 
+During this time we will compare your results against best practices.
+  You'll walk away with an idea of your overall pitch readiness including:
+  What your score was across each section.
+  How your current pItch deck fairs page by page.
+  The top three areas you should work on
+
+
 
   if (state.succeeded) {
         
     return (
       <div>
         <Type type="h1" text="What's next?"/>
-        <Type type="para1" text="We'll be in touch soon to discuss your pitch readiness."/>
-        <Type type="para1" text="Get in touch if you can't wait to improve your pitch."/>
-        <div className="mt-5"><Button url="https://calendly.com/pitchmark" title="Schedule your Pitch Readiness workshop"/></div>
+        <Type type="para1" text="Schedule your Pitch Readiness Workshop so we can compare your results against best practices."/>    
+        <Type type="para2" text="You'll walk away with an idea of your overall pitch readiness including:"/>
+        <Type type="para2" text="Where your current pitch deck is at, page by page."/>
+        <Type type="para2" text="The top three areas you should work on."/>
+        <Type type="para2" text="Feedback on why you should address certain areas"/>
+        <div className="mt-5"><Button url="https://calendly.com/pitchmark/workshop" title="Schedule your workshop"/></div>
       </div>
     );
   }
